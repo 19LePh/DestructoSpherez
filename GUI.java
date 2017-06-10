d72e9a4b2d0a142c8f725f1e49546a4dd8ce995e
19LePh <19LePh@fpsct.org>
2017-06-10 10:22:14 -0400

    GUI
    FINALLY ABLE TO SWITCH SCENES!!!!

--- GUI.java
+++ GUI.java
@@ -35,17 +35,17 @@ public class GUI extends Application
     }
 
     @Override
-    public void start(Stage primaryStage) 
+    public void start(Stage stage) 
     {
         Player player = new Player();
-        primaryStage.setTitle("Destructo Spherez!");
-        //Below makes primaryStage fullscreen
+        stage.setTitle("Destructo Spherez!");
+        //Below makes stage fullscreen
         Screen screen = Screen.getPrimary();
         Rectangle2D bounds = screen.getVisualBounds();
-        primaryStage.setX(bounds.getMinX());
-        primaryStage.setY(bounds.getMinY());
-        primaryStage.setWidth(bounds.getWidth());
-        primaryStage.setHeight(bounds.getHeight());
+        stage.setX(bounds.getMinX());
+        stage.setY(bounds.getMinY());
+        stage.setWidth(bounds.getWidth());
+        stage.setHeight(bounds.getHeight());
 
         //Creates Title Text
         DropShadow ds = new DropShadow();
@@ -69,18 +69,66 @@ public class GUI extends Application
         credits.setStyle("-fx-font: 30 Helvetica");
         equip.setStyle("-fx-font: 30 Helvetica");
 
-        //Sets the buttons sizes
+        //Changes the buttons to the same size
         launch.setMinWidth(300);
         shop.setMinWidth(300);
         records.setMinWidth(300);
         credits.setMinWidth(300);
         equip.setMinWidth(300);
 
+        //Creates the background
+        Image img = Terrain.background;
+        BackgroundImage bgimg = new BackgroundImage(img, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, new BackgroundSize(bounds.getWidth(), bounds.getHeight(), false, false, false, false));
+        Background back = new Background(bgimg);
+
+        //Creates the Title Scene and displays it
+        VBox vbox = new VBox(50); // 5 is the spacing between elements in the VBox
+        vbox.getChildren().addAll(t, launch, shop, equip, records, credits);
+        StackPane root1 = new StackPane();
+        root1.getChildren().add(vbox);
+        vbox.setAlignment(Pos.CENTER);
+        root1.setBackground(back);
+        Scene titleScene = new Scene(root1, bounds.getWidth(), bounds.getHeight());
+        stage.setScene(titleScene);
+        stage.show();
+
+        //Creates the Launch Scene
+        StackPane root2 = new StackPane();
+        //root2.getChildren().add(null);
+        root2.setBackground(back);
+        Scene launchScene = new Scene(root2, bounds.getWidth(), bounds.getHeight());
+        
+        //Creates the Shop Scene
+        StackPane root3 = new StackPane();
+        //root3.getChildren().add();
+        root3.setBackground(back);
+        Scene shopScene = new Scene(root3, bounds.getWidth(), bounds.getHeight());
+        
+        //Creates the Equip Scene
+        StackPane root4 = new StackPane();
+        //root4.getChildren().add();
+        root4.setBackground(back);
+        Scene equipScene = new Scene(root4, bounds.getWidth(), bounds.getHeight());
+        
+        //Creates the Records Scene
+        StackPane root5 = new StackPane();
+        //root5.getChildren().add(null);
+        root5.setBackground(back);
+        Scene recordsScene = new Scene(root5, bounds.getWidth(), bounds.getHeight());
+        
+        //Creates the Credits Scene
+        StackPane root6 = new StackPane();
+        //root6.getChildren().add(null);
+        root6.setBackground(back);
+        Scene creditsScene = new Scene(root6, bounds.getWidth(), bounds.getHeight());
+
+        //Code for when the buttons are clicked
         launch.setOnAction(new EventHandler<ActionEvent>()
             { 
                 @Override
                 public void handle(ActionEvent event) 
                 {
+                    stage.setScene(launchScene);
                     Terrain t = new Terrain(10, null);
                     //Switch scene
                     GameLogic.init(t, player);
@@ -88,18 +136,5 @@ public class GUI extends Application
                     Terrain.updateBackground();
                 }
             });
-        //Creates the background
-        Image img = Terrain.background;
-        BackgroundImage bgimg = new BackgroundImage(img, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, new BackgroundSize(bounds.getWidth(), bounds.getHeight(), false, false, false, false));
-        Background back = new Background(bgimg);
-        //Sets the stage
-        VBox vbox = new VBox(50); // 5 is the spacing between elements in the VBox
-        vbox.getChildren().addAll(t, launch, shop, equip, records, credits);
-        StackPane root = new StackPane();
-        root.getChildren().add(vbox);
-        vbox.setAlignment(Pos.CENTER);
-        root.setBackground(back);
-        primaryStage.setScene(new Scene(root, bounds.getWidth(), bounds.getHeight()));
-        primaryStage.show();
     }
 }
