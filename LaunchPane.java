948455713a8c0d3ace7dedf3ecfed8f7a4099426
19LePh <19LePh@fpsct.org>
2017-06-12 07:03:45 -0400

    Text  Update

--- LaunchPane.java
+++ LaunchPane.java
@@ -46,12 +46,19 @@ public class LaunchPane extends PaneBuilder
             l.setFont(Font.font("Helvetica", FontWeight.BOLD, height / 40.0));
             labels[i] = l;
         }
-        labels[0].setText("Distance: " + (int)(logic.get_x()) + " m");
+        /*labels[0].setText("Distance: " + (int)(logic.get_x()) + " m");
         labels[1].textProperty().bind(new SimpleStringProperty("Altitude: " + (int)(logic.get_y()) + " m"));
         labels[2].textProperty().bind(new SimpleStringProperty("Air Time: " + (int)(logic.getTime()) + " s"));
-        labels[3].textProperty().bind(new SimpleStringProperty("Speed: " + (int)(logic.get_vx()) + " m/s"));
+        labels[3].textProperty().bind(new SimpleStringProperty("Speed: " + (int)(logic.get_vx()) + " m/s"));*/
+        
+        labels[0].setText("Distance unavailable"); //replace this with above block of code if implmented
+        labels[1].setText("Altitude unavailable");
+        labels[2].setText("Air Time unavailable");
+        labels[3].setText("Speed unavailable");
+        
         labels[4].setText("Press Enter to Launch");
-        labels[5].setText("Press Arrow Keys to rotate");
+        //labels[5].setText("Press Arrow Keys to rotate"); Not implmented yet
+        labels[5].setText("Sprites not made; wait after launch"); //Replace this with above line if implmented
         
         //Add Labels to VBox and align
         Layout_Launch.getChildren().addAll(labels[0], labels[1], labels[2], labels[3], labels[4], labels[5]);
